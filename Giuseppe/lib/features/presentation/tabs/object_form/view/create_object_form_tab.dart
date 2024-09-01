93f15d19f2d5edc4d900e0673a37b329ed9e1c6a
Francisco-Suntaxi07 <stalynfran@hotmail.com>
2024-08-31 23:10:31 -0500

    Creacion de la tab añadir objeto

--- Giuseppe/lib/features/presentation/tabs/object_form/view/create_object_form_tab.dart
+++ Giuseppe/lib/features/presentation/tabs/object_form/view/create_object_form_tab.dart
@@ -1,13 +1,223 @@
 import 'package:flutter/material.dart';
+import 'package:giuseppe/features/presentation/common_widgets/custom_text_form_field.dart';
+import 'package:giuseppe/utils/theme/app_colors.dart';
 
 class CreateObjectFormTab extends StatelessWidget {
   const CreateObjectFormTab({super.key});
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(top: 60.0, bottom: 20.0),
+              child: const Image(
+                  image: AssetImage('assets/images/logo.png'),
+                width: 70.0,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 20.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child:  Text('AÑADIR OBJETO', style: Theme.of(context).textTheme.titleSmall),
+              ),
+            ),
+
+            Padding(padding: EdgeInsets.all(20.0), child: _NewObjectForm()),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _NewObjectForm extends StatefulWidget {
+  const _NewObjectForm({super.key});
+
+  @override
+  State<_NewObjectForm> createState() => _NewObjectFormState();
+}
+
+class _NewObjectFormState extends State<_NewObjectForm> {
+  @override
+  Widget build(BuildContext context) {
+    String? selectedCategory;
+    List<String> categories = ['Plasticos', 'Metales', 'Vidrio', 'Papel', 'Otros'];
     return Container(
-      padding: EdgeInsets.all(100.0),
-      child: Text('Formulario de objetos en contruccion'),
+      padding: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: AppColors.primaryVariantColor, width: 1),
+        borderRadius: BorderRadius.circular(10.0),
+        boxShadow: const [
+          BoxShadow(
+            color: AppColors.primaryVariantColor,
+            blurRadius: 5.0,
+          )
+        ]
+      ),
+      child: Form(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('ID', style: Theme.of(context).textTheme.bodyMedium),
+                        CustomTextFormField(
+                          formFieldType: FormFieldType.id,
+                          hintText: '12345',
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(width: 20.0),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Cantidad', style: Theme.of(context).textTheme.bodyMedium),
+                        CustomTextFormField(
+                          formFieldType: FormFieldType.quantity,
+                          hintText: '2',
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20.0),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Nombre', style: Theme.of(context).textTheme.bodyMedium),
+                  CustomTextFormField(
+                      formFieldType: FormFieldType.name,
+                      hintText: 'Nombre objeto')
+                ],
+              ),
+              const SizedBox(height: 20.0),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Descripción', style: Theme.of(context).textTheme.bodyMedium),
+                  CustomTextFormField(
+                      formFieldType: FormFieldType.description,
+                      hintText: 'Nombre objeto')
+                ],
+              ),
+              const SizedBox(height: 20.0),
+              Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Estado', style: Theme.of(context).textTheme.bodyMedium),
+                        CustomTextFormField(
+                          formFieldType: FormFieldType.status,
+                          hintText: 'disponible',
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(width: 20.0),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Categoria', style: Theme.of(context).textTheme.bodyMedium),
+                        const SizedBox(height: 5.0),
+                        DropdownMenu(
+                          initialSelection: selectedCategory,
+                          inputDecorationTheme: InputDecorationTheme(
+                            border: const OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                              borderSide: const BorderSide(
+                                color: AppColors.primaryVariantColor,
+                                width: 1.0,
+                              ),
+                            ),
+                          ),
+                          hintText: 'Seleccione una categoría',
+                          onSelected: (String? value) {
+                            setState(() {
+                              selectedCategory = value;
+                            });
+                            print('Categoría seleccionada: $value');
+                          },
+                          dropdownMenuEntries: categories.map((String category) {
+                            return DropdownMenuEntry<String>(
+                              value: category,
+                              label: category,
+                            );
+                          }).toList(),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+
+              Container(
+                padding: const EdgeInsets.only(top: 30.0, bottom: 20.0),
+                child: ElevatedButton(
+                  onPressed: () async {
+                  },
+                  child: const Text('Guardar'),
+                ),
+              ),
+            ],
+          )
+      )
+    );
+  }
+}
+
+class CategoryDropdown extends StatelessWidget {
+  final String? selectedCategory;
+  final Function(String?) onCategorySelected;
+  final List<String> categories;
+
+  const CategoryDropdown({
+    super.key,
+    required this.selectedCategory,
+    required this.onCategorySelected,
+    required this.categories,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        DropdownMenu(
+          initialSelection: selectedCategory,
+          inputDecorationTheme: const InputDecorationTheme(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+            ),
+          ),
+          hintText: 'Seleccione una categoría',
+          onSelected: (String? value) {
+            onCategorySelected(value); // Llama a la función de callback para actualizar la categoría
+          },
+          dropdownMenuEntries: categories.map((String category) {
+            return DropdownMenuEntry<String>(
+              value: category,
+              label: category,
+            );
+          }).toList(),
+        ),
+      ],
     );
   }
 }
